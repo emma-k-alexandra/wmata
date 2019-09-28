3e028e5cfb19db5802422531081a5d77359a6240
Emma Foster <emma@emma.sh>
2019-09-28 12:41:44 -0400

    Happy path tests for Station

--- src/station/tests.rs
+++ src/station/tests.rs
@@ -29,3 +29,36 @@ fn test_parking_information() {
         assert_eq!(x.stations_parking.len(), 0);
     });
 }
+
+#[test]
+fn test_path_to_station() {
+    let station = Station::new("9e38c3eab34c4e6c990828002828f5ed", StationCode::A01);
+
+    station.path(StationCode::A02, |x| {
+        let x = x.unwrap();
+
+        assert_eq!(x.path[1].distance_to_previous_station, 4178)
+    });
+}
+
+#[test]
+fn test_timings() {
+    let station = Station::new("9e38c3eab34c4e6c990828002828f5ed", StationCode::A01);
+
+    station.timings(|x| {
+        let x = x.unwrap();
+
+        assert_eq!(x.station_times[0].code, "A01");
+    })
+}
+
+#[test]
+fn test_to_station() {
+    let station = Station::new("9e38c3eab34c4e6c990828002828f5ed", StationCode::A01);
+
+    station.to_station(StationCode::A02, |x| {
+        let x = x.unwrap();
+
+        assert_eq!(x.station_to_station_infos[0].source_station, "A01");
+    })
+}
