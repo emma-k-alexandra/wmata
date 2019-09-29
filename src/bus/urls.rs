23b4e138952db72192b2f0804188a6385e832e31
Emma Foster <emma@emma.sh>
2019-09-29 16:44:26 -0400

    Bus URLs enum

--- src/bus/urls.rs
+++ src/bus/urls.rs
@@ -1,1 +1,25 @@
+pub enum URLs {
+    Routes,
+    Stops,
+    Incidents,
+    Positions,
+    PathDetails,
+    RouteSchedule,
+    NextBuses,
+    StopSchedule
+}
 
+impl ToString for URLs {
+    fn to_string(&self) -> String {
+        match self {
+            URLs::Routes => "https://api.wmata.com/Bus.svc/json/jRoutes".to_string(),
+            URLs::Stops => "https://api.wmata.com/Bus.svc/json/jStops".to_string(),
+            URLs::Incidents => "https://api.wmata.com/Incidents.svc/json/BusIncidents".to_string(),
+            URLs::Positions => "https://api.wmata.com/Bus.svc/json/jBusPositions".to_string(),
+            URLs::PathDetails => "https://api.wmata.com/Bus.svc/json/jRouteDetails".to_string(),
+            URLs::RouteSchedule => "https://api.wmata.com/Bus.svc/json/jRouteSchedule".to_string(),
+            URLs::NextBuses => "https://api.wmata.com/NextBusService.svc/json/jPredictions".to_string(),
+            URLs::StopSchedule => "https://api.wmata.com/Bus.svc/json/jStopSchedul".to_string(),
+        }
+    }
+}
